564ac24ea78a4b102b4a9fae3a20df35a2d0d684
David Tielke <mail@david-tielke.de>
2024-06-19 12:42:28 +0200

    - DI Kernel

--- PersonManagerApp/Program.cs
+++ PersonManagerApp/Program.cs
@@ -2,6 +2,7 @@
 using DavidTielke.PMA.Data.DataStorage;
 using DavidTielke.PMA.Data.FileStorage;
 using DavidTielke.PMA.Logic.PersonManagement;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DavidTielke.PMA.UI.ConsoleClient
 {
@@ -10,11 +11,16 @@ namespace DavidTielke.PMA.UI.ConsoleClient
 
         static void Main(string[] args)
         {
-            var reader = new FileReader();
-            var parser = new PersonParser();
-            var repo = new PersonRepository(parser, reader);
-            var manager = new PersonManager(repo);
-            var commands = new PersonCommands(manager);
+            var collection = new ServiceCollection();
+            collection.AddTransient<IPersonCommands, PersonCommands>();
+            collection.AddTransient<IPersonManager, PersonManager>();
+            collection.AddTransient<IPersonRepository, PersonRepository>();
+            collection.AddTransient<IPersonParser, PersonParser>();
+            collection.AddTransient<IFileReader, FileReader>();
+
+            var provider = collection.BuildServiceProvider();
+
+            var commands = provider.GetRequiredService<IPersonCommands>();
 
             commands.DisplayAllAdults();
             commands.DisplayAllChildren();
